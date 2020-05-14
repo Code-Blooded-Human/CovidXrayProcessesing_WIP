390474c7b197d3836a6ddf15d9f1917cab8fa724
Abhishek <abhisheks@iitbhilai.ac.in>
2020-05-14 15:45:42 +0530

    Draw bounding boxes on specified image with scaling ( assumes 224 * 224 px reference images and scales the coordinates for actual image )

--- drawBoundingBox.py
+++ drawBoundingBox.py
@@ -1,10 +1,11 @@
 # versiom 0.0.1
+# Draw bounding boxes on JPEG images 
 # author: Abhishek Shingane abhisheks@iitbhilai.ac.in
 # Requirements
-# Pillow: 
-#   Image Manipulation Library forked from PIL 
+# Pillow:
+#   Image Manipulation Library forked from PIL
 #   pip3 install --upgrade Pillow
-#
+
 
 import sys
 import os
@@ -21,36 +22,19 @@ def scaleCoordinates(boundingCoordinates,imageSize,referenceImageSize):
     newBoundingCoordinates[2] = (imageSize[0]*boundingCoordinates[2])/referenceImageSize[0]
     newBoundingCoordinates[3] = (imageSize[1]*boundingCoordinates[3])/referenceImageSize[1]
     return newBoundingCoordinates
-        
+
 
 def drawBoundingBox(srcUrl,boundingCoordinates,destUrl="out.jpeg",referenceImageSize=standardReferenceImageSize):
     try:
         rawImage = Image.open(srcUrl)
     except FileNotFoundError as err:
         print("Couldnot open or find file: "+srcUrl)
-        exit()        
+        exit()
     imageSize=[rawImage.size[0],rawImage.size[1]] #Width Height
     resizedBoundingCoordinates = scaleCoordinates(boundingCoordinates,imageSize,referenceImageSize)
     draw = ImageDraw.Draw(rawImage)
     draw.rectangle(resizedBoundingCoordinates)
     del draw
-    rawImage.save(destUrl)   
+    rawImage.save(destUrl)
 
 drawBoundingBox("./assets/test_xra831x1024.jpeg",[10,10,100,100],"out.jpeg")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
